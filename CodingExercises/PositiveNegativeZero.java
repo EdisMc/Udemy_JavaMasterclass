d8600924e834005babf698eee071826922dd4c7d
Edis Reyhan <92871901+EdisMc@users.noreply.github.com>
2022-04-05 20:21:01 +0300

    Add files via upload

---
+++ CodingExercises/PositiveNegativeZero.java
@@ -0,0 +1,25 @@
+package SampleCodingExercise;
+
+public class PositiveNegativeZero {
+    public static void main(String[] args) {
+        checkNumber(5);
+        checkNumber(-4);
+        checkNumber(0);
+        checkNumber(3378);
+        checkNumber(0);
+        checkNumber(-8565445);
+        checkNumber(-3412);
+        checkNumber(457405358);
+    }
+
+    public static void checkNumber(int number) {
+        if (number > 0) {
+            System.out.println("positive");
+        } else if(number < 0) {
+            System.out.println("negative");
+        } else {
+            System.out.println("zero");
+        }
+    }
+
+}
