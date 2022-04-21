07379f3b65441c277e29adadabef031b6b363738
Edis Reyhan <92871901+EdisMc@users.noreply.github.com>
2022-04-21 19:18:47 +0300

    Create First&LastDigit.java

---
+++ CodingExercises/ControlFlow - Exercises/First&LastDigit.java
@@ -0,0 +1,20 @@
+public class FirstAndLastDigitSum {
+
+    public static int sumFirstAndLastDigit(int number) {
+        int lastDigit = 0;
+        if (number >= 0) {
+            lastDigit = number % 10;
+
+            while (number > 9) {
+                number = number / 10;
+            }
+
+            System.out.println("First Number " + number + " Last Number " + lastDigit + " Total = " + (number + lastDigit));
+            return 0;
+        } else {
+        System.out.println("-1");
+        return -1;
+        }
+    }
+
+}
