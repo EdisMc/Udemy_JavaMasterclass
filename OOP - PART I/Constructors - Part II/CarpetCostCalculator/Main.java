006366d98d855539598827b4f08bc766c0480008
Edis Reyhan <92871901+EdisMc@users.noreply.github.com>
2022-04-22 20:33:04 +0300

    Create Main.java

---
+++ OOP - PART I/Constructors - Part II/CarpetCostCalculator/Main.java
@@ -0,0 +1,16 @@
+package coding_exercise33;
+
+public class Main {
+    public static void main(String[] args) {
+        Carpet carpet = new Carpet(13.5);
+        Floor floor = new Floor(2.75, 4.0);
+        Calculator calculator = new Calculator(floor, carpet);
+
+        System.out.println("Total cost = " + calculator.getTotalCost());
+
+        carpet = new Carpet(5.5);
+        floor = new Floor(5.4, 4.5);
+        calculator = new Calculator(floor, carpet);
+
+        System.out.println("Total cost = " + calculator.getTotalCost());
+    }
