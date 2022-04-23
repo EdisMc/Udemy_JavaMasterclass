d96dc77185c0bcde9459cddba4f035b2555fc38d
Edis Reyhan <92871901+EdisMc@users.noreply.github.com>
2022-04-23 20:49:55 +0300

    Create Main.java

---
+++ OOP - PART I/Inheritance - Part I/challenge/Main.java
@@ -0,0 +1,9 @@
+package inheritance_part1.challenge;
+
+public class Main {
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle("CAR", "2000");
+        vehicle.move(5, 10);
+
+    }
+}
