41c8f5175881f00b42555dfbc4ef79e54dce32db
Edis Reyhan <92871901+EdisMc@users.noreply.github.com>
2022-04-23 20:59:40 +0300

    Add files via upload

---
+++ OOP - PART I/Inheritance - Part I/Cylinder/Circle.java
@@ -0,0 +1,22 @@
+package coding_exercise35;
+
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        if (radius <0){
+            this.radius = 0;
+        }else{
+            this.radius = radius;
+        }
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea(){
+        return radius*radius*Math.PI;
+    }
+
+}
