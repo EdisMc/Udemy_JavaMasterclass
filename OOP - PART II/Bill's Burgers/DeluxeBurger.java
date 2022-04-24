f720ba2265291557bec152a6ca3be1b0f5b3ca44
Edis Reyhan <92871901+EdisMc@users.noreply.github.com>
2022-04-24 20:11:18 +0300

    Add files via upload

---
+++ OOP - PART II/Bill's Burgers/DeluxeBurger.java
@@ -0,0 +1,29 @@
+package coding_exercise40;
+
+public class DeluxeBurger extends Hamburger {
+    public DeluxeBurger() {
+        super("Deluxe Hamburger", "white", "beef & bacon", 8.50);
+        super.addition1("Chips", 1.50);
+        super.addition2("Drinks",2.34);
+    }
+
+    @Override
+    public void addition1(String additionName, double additionPrice) {
+        System.out.println("Item not available.");
+    }
+
+    @Override
+    public void addition2(String additionName, double additionPrice) {
+        System.out.println("Item not available.");
+    }
+
+    @Override
+    public void addition3(String additionName, double additionPrice) {
+        System.out.println("Item not available.");
+    }
+
+    @Override
+    public void addition4(String additionName, double additionPrice) {
+        System.out.println("Item not available.");
+    }
+}
