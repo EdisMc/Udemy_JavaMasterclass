8840b0d5c89b8719537cf1337044569f8397cf4f
Edis Reyhan <92871901+EdisMc@users.noreply.github.com>
2022-05-08 21:01:43 +0300

    Add files via upload

---
+++ InnerAndAbstractClasses&Interfaces/Interfaces/part3/Gearbox.java
@@ -0,0 +1,12 @@
+package Interfaces.part3;
+
+public class Gearbox {
+
+    private boolean clutchIsIn;
+
+    public void operateClutch(boolean inOrOut) {
+        this.clutchIsIn = inOrOut;
+    }
+
+}
+
